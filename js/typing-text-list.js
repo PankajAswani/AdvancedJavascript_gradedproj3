73705a4426773c3de60d8fa119bb392286132bd4
Pankaj Aswani <developerpanks92@gmail.com>
2022-06-11 18:02:15 +0530

    Completed Javascript Assignment

---
+++ js/typing-text-list.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const typingTextList = [
+    'Take up one idea',
+    'Make that one idea your life',
+    'Think of it',
+    'Dream of it',
+    'Live on that idea',
+    'Let every part of your body be full of that idea',
+    'And just leave every other idea alone',
+    'This is the way to success'
+];
